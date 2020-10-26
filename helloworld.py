c214264b459c10249179055423ad015857802608
ninetyfour2010 <ninetyfour@163.com>
2020-10-26 08:01:27 +0000

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class hw(object):
+    def __init__(self):
+      pass
+
+    def hwp(self):
+      print("hello,world !")
+
+if __name__ == '__main__':
+
+  pp = hw()
+  pp.hwp()
